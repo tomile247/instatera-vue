afd122cce9f6e103526b404cacf64e0574dd61c7
Tomi Levkus <tomi.levkus@gmail.com>
2025-06-10 20:33:50 +0200

    Build fix

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -8,14 +8,14 @@ import { onMounted, ref } from 'vue'
 const mapRef = ref<HTMLDivElement | null>(null)
 
 onMounted(() => {
-  if (window.google && mapRef.value) {
-    const map = new google.maps.Map(mapRef.value, {
+  if ((window as any).google && mapRef.value) {
+    const map = new (window as any).google.maps.Map(mapRef.value, {
       center: {lat: 45.81704002979856, lng: 16.031093702207404},
       zoom: 16,
       disableDefaultUI: true,
     })
 
-    new google.maps.Marker({
+    new (window as any).google.maps.Marker({
       position: {lat: 45.81704002979856, lng: 16.031093702207404},
       map,
       title: 'Zagreb Office',
