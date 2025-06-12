330f0dd2c37cd9c226d4673da5d43bbbb96296aa
Tomi Levkus <tomi.levkus@gmail.com>
2025-06-12 18:16:30 +0200

    Style changes on services cards

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,56 +1,92 @@
 <template>
   <div class="services-container">
-    <router-link to="/bowen" class="service-slide" :style="{ backgroundImage: `url(${bowenImage})`}">
+    <router-link to="/bowen" class="service-slide">
+      <img :src="bowenImage">
       <span class="title">BOWEN</span>
-      <span>Saznaj više</span>
+      <div class="description">
+        <p class="price">Cijena od 35 €</p>
+        <p class="time">Vrijeme trajanja tretmana od 30 do 60 minuta</p>
+        <button>Saznaj više</button>
+      </div>
     </router-link>
 
-    <router-link to="/baby-bowen" class="service-slide" :style="{ backgroundImage: `url(${bowenImage})`}">
+    <router-link to="/baby-bowen" class="service-slide">
+      <img :src="bowenImage">
       <span class="title">BABY BOWEN</span>
-      <span>Saznaj više</span>
+      <div class="description">
+        <p class="price">Cijena od 15 €</p>
+        <p class="time">Vrijeme trajanja tretmana od 20 do 40 minuta</p>
+        <button>Saznaj više</button>
+      </div>
     </router-link>
 
-    <router-link to="/tecar" class="service-slide" :style="{ backgroundImage: `url(${tecarImage})`}">
+    <router-link to="/tecar" class="service-slide">
+      <img :src="tecarImage">
       <span class="title">TECAR TERAPIJA</span>
-      <span>Saznaj više</span>
+      <div class="description">
+        <p class="price">Cijena od 20 €</p>
+        <p class="time">Vrijeme trajanja tretmana od 20 do 40 minuta</p>
+        <button>Saznaj više</button>
+      </div>
     </router-link>
 
-    <router-link to="/terapijske-vjezbe" class="service-slide" :style="{ backgroundImage: `url(${terapijaImage})`}">
-        <span class="title">TERAPIJSKE VJEŽBE</span>
-        <span>Saznaj više</span>
+    <router-link to="/terapijske-vjezbe" class="service-slide">
+      <img :src="terapijaImage">
+      <span class="title">TERAPIJSKE VJEŽBE</span>
+      <div class="description">
+        <p class="price">Cijena od 20 €</p>
+        <p class="time">Vrijeme trajanja tretmana od 30 do 60 minuta</p>
+        <button>Saznaj više</button>
+      </div>
     </router-link>
 
-    <router-link to="/medicinska" class="service-slide" :style="{ backgroundImage: `url(${medicinskaImage})`}">
+    <router-link to="/medicinska" class="service-slide">
+      <img :src="medicinskaImage">
       <span class="title">MEDICINSKA MASAŽA</span>
-      <span>Saznaj više</span>
+      <div class="description">
+        <p class="price">Cijena od 25 €</p>
+        <p class="time">Vrijeme trajanja tretmana od 30 do 60 minuta</p>
+        <button>Saznaj više</button>
+      </div>
     </router-link>
 
-    <router-link to="/deep-lifting" class="service-slide" :style="{ backgroundImage: `url(${liceImage})`}">
+    <router-link to="/deep-lifting" class="service-slide">
+      <img :src="liceImage">
       <span class="title">DEEP LIFTING</span>
-      <span>Saznaj više</span>
+      <div class="description">
+        <p class="price">Cijena od 40 €</p>
+        <p class="time">Vrijeme trajanja tretmana 35 minuta</p>
+        <button>Saznaj više</button>
+      </div>
     </router-link>
 
-    <router-link to="/bukalna" class="service-slide" :style="{ backgroundImage: `url(${liceImage})`}">
+    <router-link to="/bukalna" class="service-slide">
+      <img :src="liceImage">
       <span class="title">BUKALNA MASAŽA</span>
-      <span>Saznaj više</span>
+      <div class="description">
+        <p class="price">Cijena od 25 €</p>
+        <p class="time">Vrijeme trajanja tretmana 20 minuta</p>
+        <button>Saznaj više</button>
+      </div>
     </router-link>
 
-    <router-link to="/radiofrekvencija-lica" class="service-slide" :style="{ backgroundImage: `url(${liceImage})`}">
+    <router-link to="/radiofrekvencija-lica" class="service-slide">
+      <img :src="liceImage">
       <span class="title">RADIOFREKVENCIJA LICA</span>
-      <span>Saznaj više</span>
+      <div class="description">
+        <p class="price">Cijena od 30 €</p>
+        <p class="time">Vrijeme trajanja tretmana 30 minuta</p>
+        <button>Saznaj više</button>
+      </div>
     </router-link>
 
-    <router-link to="/tretman-oziljaka" class="service-slide" :style="{ backgroundImage: `url(${oziljakImage})`}">
-      <div>
-        <span>Primjenjuje se kod:
-
-
-          <ul>
-            <li></li>
-          </ul>
-        </span>
-        <span class="title">TRETMAN OŽILJKA</span>
-        <span>Saznaj više</span>
+    <router-link to="/tretman-oziljaka" class="service-slide">
+      <img :src="oziljakImage">
+      <span class="title">TRETMAN OŽILJKA</span>
+      <div class="description">
+        <p class="price">Cijena od 20 €</p>
+        <p class="time">Vrijeme trajanja tretmana od 30 do 60 minuta</p>
+        <button>Saznaj više</button>
       </div>
     </router-link>
   </div>
@@ -86,9 +122,7 @@ import terapijaImage from '@/assets/terapija.jpg';
   background-position: center;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
-  justify-content: flex-end;
-  padding: 20px;
+  padding: 10px;
   box-sizing: border-box;
   border-radius: 15px;
   transition: all 200ms ease-in-out;
@@ -98,20 +132,49 @@ import terapijaImage from '@/assets/terapija.jpg';
   .title {
     backdrop-filter: blur(10px);
     font-size: 1.8rem;
-    color: white;
     font-weight: 500;
     border-radius: 15px;
     display: flex;
-    justify-content: center;
+    justify-content: flex-start;
     width: 100%;
+    color: black;
   }
+
+  .description {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    transition: all 250ms ease-in-out;
+    visibility: hidden;
+    opacity: 0;
+    height: 0;
+  }
+}
+
+img {
+  transition: all 250ms ease-in-out;
+  width: 100%;
+  object-fit: cover;
+  border-radius: 15px;
+  max-height: 80%;
 }
 
 .service-slide:hover {
   transform: scale(1.05);
+
+  img {
+    width: 100%;
+    object-fit: cover;
+    border-radius: 15px;
+    max-height: 40%;
+  }
+
+  .description {
+    visibility: visible;
+    opacity: 1;
+    height: 60%;
+  }
 }
 
-img {
-  width: 100%;
-}
+
 </style>
