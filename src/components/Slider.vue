2cd43c4a1c579800ed5538aecbdb430477589e37
Tomi Levkus <tomi.levkus@gmail.com>
2025-06-12 19:45:29 +0200

    Style changes on services cards

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -2,90 +2,119 @@
   <div class="services-container">
     <router-link to="/bowen" class="service-slide">
       <img :src="bowenImage">
-      <span class="title">BOWEN</span>
+      <span class="title">Bowen</span>
       <div class="description">
         <p class="price">Cijena od 35 €</p>
-        <p class="time">Vrijeme trajanja tretmana od 30 do 60 minuta</p>
+        <p class="time">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>
+          od 30 do 60 minuta
+        </p>
         <button>Saznaj više</button>
       </div>
     </router-link>
 
     <router-link to="/baby-bowen" class="service-slide">
       <img :src="bowenImage">
-      <span class="title">BABY BOWEN</span>
+      <span class="title">Baby bowen</span>
       <div class="description">
-        <p class="price">Cijena od 15 €</p>
-        <p class="time">Vrijeme trajanja tretmana od 20 do 40 minuta</p>
+        <p class="price">
+          Cijena od 15 €
+        </p>
+        <p class="time">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>
+          od 20 do 40 minuta
+        </p>
         <button>Saznaj više</button>
       </div>
     </router-link>
 
     <router-link to="/tecar" class="service-slide">
       <img :src="tecarImage">
-      <span class="title">TECAR TERAPIJA</span>
+      <span class="title">Tecar</span>
       <div class="description">
         <p class="price">Cijena od 20 €</p>
-        <p class="time">Vrijeme trajanja tretmana od 20 do 40 minuta</p>
+        <p class="time">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>
+          od 20 do 40 minuta
+        </p>
         <button>Saznaj više</button>
       </div>
     </router-link>
 
     <router-link to="/terapijske-vjezbe" class="service-slide">
       <img :src="terapijaImage">
-      <span class="title">TERAPIJSKE VJEŽBE</span>
+      <span class="title">Terapijske vježbe</span>
       <div class="description">
         <p class="price">Cijena od 20 €</p>
-        <p class="time">Vrijeme trajanja tretmana od 30 do 60 minuta</p>
+        <p class="time">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>
+          od 30 do 60 minuta
+        </p>
         <button>Saznaj više</button>
       </div>
     </router-link>
 
     <router-link to="/medicinska" class="service-slide">
       <img :src="medicinskaImage">
-      <span class="title">MEDICINSKA MASAŽA</span>
+      <span class="title">Medicinska masaža</span>
       <div class="description">
         <p class="price">Cijena od 25 €</p>
-        <p class="time">Vrijeme trajanja tretmana od 30 do 60 minuta</p>
+        <p class="time">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>
+          od 30 do 60 minuta
+        </p>
         <button>Saznaj više</button>
       </div>
     </router-link>
 
     <router-link to="/deep-lifting" class="service-slide">
       <img :src="liceImage">
-      <span class="title">DEEP LIFTING</span>
+      <span class="title">Deep lifting</span>
       <div class="description">
         <p class="price">Cijena od 40 €</p>
-        <p class="time">Vrijeme trajanja tretmana 35 minuta</p>
+        <p class="time">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>
+          35 minuta
+        </p>
         <button>Saznaj više</button>
       </div>
     </router-link>
 
     <router-link to="/bukalna" class="service-slide">
       <img :src="liceImage">
-      <span class="title">BUKALNA MASAŽA</span>
+      <span class="title">Bukalna masaža</span>
       <div class="description">
         <p class="price">Cijena od 25 €</p>
-        <p class="time">Vrijeme trajanja tretmana 20 minuta</p>
+        <p class="time">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>
+          20 minuta
+        </p>
         <button>Saznaj više</button>
       </div>
     </router-link>
 
     <router-link to="/radiofrekvencija-lica" class="service-slide">
       <img :src="liceImage">
-      <span class="title">RADIOFREKVENCIJA LICA</span>
+      <span class="title">Radiofrekvencija lica</span>
       <div class="description">
         <p class="price">Cijena od 30 €</p>
-        <p class="time">Vrijeme trajanja tretmana 30 minuta</p>
+        <p class="time">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>
+          30 minuta
+        </p>
         <button>Saznaj više</button>
       </div>
     </router-link>
 
     <router-link to="/tretman-oziljaka" class="service-slide">
       <img :src="oziljakImage">
-      <span class="title">TRETMAN OŽILJKA</span>
+      <span class="title">Tretman ožiljaka</span>
       <div class="description">
         <p class="price">Cijena od 20 €</p>
-        <p class="time">Vrijeme trajanja tretmana od 30 do 60 minuta</p>
+        <p class="time">
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z"/></svg>
+          od 30 do 60 minuta
+        </p>
         <button>Saznaj više</button>
       </div>
     </router-link>
@@ -109,25 +138,35 @@ import terapijaImage from '@/assets/terapija.jpg';
 }
 
 .services-container {
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
   gap: 20px;
   flex-wrap: wrap;
+  max-width: 100vw;
+
+  @media screen and (min-width: 640px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (min-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
 }
 
 .service-slide {
-  flex: 1 1 30%;
-  min-width: 30%;
+  max-width: 100%;
   height: 400px;
   background-size: cover;
   background-position: center;
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   padding: 10px;
   box-sizing: border-box;
-  border-radius: 15px;
   transition: all 200ms ease-in-out;
   -webkit-box-shadow: 0px 15px 50px 0px rgba(27, 32, 50, 0.1);
   box-shadow: 0px 15px 50px 0px rgba(27, 32, 50, 0.1);
+  color: black;
 
   .title {
     backdrop-filter: blur(10px);
@@ -149,11 +188,28 @@ import terapijaImage from '@/assets/terapija.jpg';
     opacity: 0;
     height: 0;
   }
+
+  .price {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    font-size: 1.2rem;
+  }
+
+  .time {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
+
+  button {
+    width: 50%;
+  }
 }
 
 img {
   transition: all 250ms ease-in-out;
-  width: 100%;
+  width: auto;
   object-fit: cover;
   border-radius: 15px;
   max-height: 80%;
@@ -163,8 +219,7 @@ img {
   transform: scale(1.05);
 
   img {
-    width: 100%;
-    object-fit: cover;
+    width: auto;
     border-radius: 15px;
     max-height: 40%;
   }
@@ -172,7 +227,7 @@ img {
   .description {
     visibility: visible;
     opacity: 1;
-    height: 60%;
+    height: 50%;
   }
 }
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -108,6 +108,19 @@ import intro from '@/assets/intro.jpg';
 
 <style scoped>
 
+.portret__image {
+  animation: morph 5s infinite alternate ease-in-out;
+}
+
+@keyframes morph {
+  0% {
+    clip-path: shape(from 94.06% 44.72%,curve to 94.03% 55.27% with 93.54% 50.00%,curve to 90.35% 64.36% with 94.51% 60.55%,curve to 82.46% 71.14% with 86.19% 68.18%,curve to 77.79% 80.09% with 78.73% 74.11%,curve to 71.17% 85.99% with 76.85% 86.07%,curve to 60.97% 86.23% with 65.49% 85.91%,curve to 51.99% 89.33% with 56.44% 86.54%,curve to 42.56% 91.82% with 47.55% 92.13%,curve to 33.04% 89.37% with 37.57% 91.50%,curve to 24.01% 84.77% with 28.51% 87.23%,curve to 16.79% 77.97% with 19.51% 82.31%,curve to 9.00% 70.20% with 14.07% 73.63%,curve to 4.08% 61.06% with 3.93% 66.77%,curve to 7.25% 50.35% with 4.23% 55.35%,curve to 7.56% 39.46% with 10.26% 45.36%,curve to 10.03% 30.34% with 4.85% 33.57%,curve to 17.12% 22.15% with 15.22% 27.12%,curve to 22.47% 12.72% with 19.02% 17.17%,curve to 32.08% 9.52% with 25.91% 8.27%,curve to 43.06% 11.97% with 38.26% 10.78%,curve to 52.82% 9.51% with 47.86% 13.17%,curve to 62.28% 8.49% with 57.78% 5.85%,curve to 73.21% 10.64% with 66.77% 11.12%,curve to 83.70% 14.25% with 79.66% 10.16%,curve to 87.76% 24.68% with 87.74% 18.33%,curve to 91.18% 35.23% with 87.78% 31.03%,curve to 94.06% 44.72% with 94.57% 39.44%);
+  }
+  to {
+    clip-path: shape(from 94.30% 44.61%,curve to 91.00% 54.60% with 93.15% 50.00%,curve to 91.75% 65.82% with 88.85% 59.21%,curve to 87.48% 73.92% with 94.66% 72.43%,curve to 78.37% 80.47% with 80.30% 75.42%,curve to 72.77% 89.90% with 76.45% 85.53%,curve to 63.41% 94.02% with 69.10% 94.28%,curve to 52.79% 90.26% with 57.72% 93.77%,curve to 42.27% 90.61% with 47.86% 86.76%,curve to 33.77% 88.81% with 36.69% 94.46%,curve to 26.18% 81.68% with 30.86% 83.16%,curve to 18.20% 76.64% with 21.51% 80.20%,curve to 13.91% 68.29% with 14.89% 73.09%,curve to 6.76% 59.64% with 12.92% 63.50%,curve to 8.15% 50.88% with 0.59% 55.77%,curve to 11.11% 40.08% with 15.71% 45.99%,curve to 9.98% 30.07% with 6.51% 34.17%,curve to 17.14% 22.52% with 13.45% 25.96%,curve to 26.53% 19.16% with 20.84% 19.09%,curve to 36.15% 18.03% with 32.23% 19.22%,curve to 43.59% 8.57% with 40.07% 16.84%,curve to 52.14% 4.80% with 47.11% 0.30%,curve to 63.04% 7.74% with 57.18% 9.30%,curve to 70.80% 12.85% with 68.90% 6.18%,curve to 77.99% 20.79% with 72.70% 19.51%,curve to 88.88% 24.87% with 83.29% 22.07%,curve to 94.96% 33.45% with 94.47% 27.67%,curve to 94.30% 44.61% with 95.45% 39.23%);
+  }
+}
+
 section {
   border-radius: 15px;
 }
@@ -192,9 +205,18 @@ span.highlight {
 
 #rezervacije {
   article {
+    animation: morph 3s infinite alternate ease-in-out;
+    height: 90vh;
+    background-color: var(--color-green-dark);
+    text-align: center;
+    font-size: 1.2rem;
+    color: var(--color-text);
+    font-weight: 600;
     padding: 50px;
     display: flex;
+    flex-direction: column;
     align-items: center;
+    justify-content: center;
     gap: 50px;
     border-radius: 15px;
   }
